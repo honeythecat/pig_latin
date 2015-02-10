40032a9f6c4444ffe4e862f82c22475efc4537e8
Heather Anderson & Dexter Fitch <student@epicodus.com>
2015-02-09 16:50:58 -0800

    Add continued functionality with known bugs.

--- js/scripts.js
+++ js/scripts.js
@@ -11,15 +11,25 @@ var containsVowel = function(character) {
 };
 
 var pigLatin = function(phrase) {
-  var splitWord = phrase.split('');
-  var firstLetter = phrase.slice(0,1);
+  var splitWord = phrase.split("");
+
+  for (var index = 0; index < splitWord.length; index += 1) {
+    if (containsVowel(splitWord[index])) {
+      var vowelPosition = index;
+    } else {
+      "No vowels";
+    }
+  }
+
+  var firstConsonants = phrase.slice(0, vowelPosition);
 
   if (containsVowel(splitWord[0])) {
     return phrase.concat("ay");
-  } else if (!containsVowel(splitWord[0])) {
-    return phrase.slice(1).concat(firstLetter).concat("ay");
+  } else if ((containsVowel(splitWord[vowelPosition]))) {
+    return phrase.slice(vowelPosition).concat(firstConsonants).concat("ay");
   }
 };
 
-
-// phrase.slice(0, 1);
+// Really close! Check out 
+// https://gist.github.com/BobbySig/6149927
+// for some hints
--- spec/specs.js
+++ spec/specs.js
@@ -7,4 +7,8 @@ describe('pigLatin', function() {
     expect(pigLatin("cat")).to.equal("atcay");
   });
 
+  it("shifts all the first letters if they are consonants to the end of a word and adds 'ay'", function() {
+    expect(pigLatin("stutter")).to.equal("utterstay");
+  });
+
 });
